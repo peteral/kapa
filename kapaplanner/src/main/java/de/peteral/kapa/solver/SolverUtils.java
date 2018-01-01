4086357ec18da4d56a8bc94b9531b6344a242a69
peteral <ladislav.petera@yahoo.de>
2018-01-01 11:33:33 +0100

    no bonus for earlier due projects

--- kapaplanner/src/main/java/de/peteral/kapa/solver/SolverUtils.java
+++ kapaplanner/src/main/java/de/peteral/kapa/solver/SolverUtils.java
@@ -7,7 +7,9 @@ public class SolverUtils {
         private static List<String> sprints = Collections.emptyList();
 
     public static int getDelayCosts(String due, String lastSprint, int costsPerSprint) {
-        return (sprints.indexOf(lastSprint) - sprints.indexOf(due)) * costsPerSprint;
+        int result = (sprints.indexOf(lastSprint) - sprints.indexOf(due)) * costsPerSprint;
+        // there is no "bonus" for finishing too early
+        return (result > 0) ? result : 0;
     }
 
     public static void injectSprints(List<String> sprints) {
